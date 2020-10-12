b5f322aa82babf7579ee3bfd060dd779bd957296
Nurbek <musin.nurbek@inbox.ru>
2020-10-12 14:11:12 +0600

    feat: implemented the function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+ return (matrix || []).reduce((accum, currentElement, idx) => accum = [...accum, ...(idx % 2 ? currentElement.reverse() : currentElement)], []);
 }
